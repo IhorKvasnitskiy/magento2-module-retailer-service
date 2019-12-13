d07361cb106c59085210d9d7ab15ad8dc6eb36ec
Ihor KVASNYTSKYI <ihor.kvasnytskyi@smile-ukraine.com>
2019-12-13 15:12:29 +0200

    ihkva_Change#821992 change filters and add sortOrder for services

--- Plugin/StoreLocatorBlockViewMapPlugin.php
+++ Plugin/StoreLocatorBlockViewMapPlugin.php
@@ -13,8 +13,13 @@
 namespace Smile\RetailerService\Plugin;
 
 use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Api\SortOrder;
+use Smile\RetailerPromotion\Api\Data\PromotionInterface;
 use Smile\RetailerService\Api\ServiceRepositoryInterface;
 use Smile\RetailerService\Api\Data\ServiceInterface;
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\Search\FilterGroupBuilder;
+use Magento\Framework\Api\SortOrderBuilder;
 
 class StoreLocatorBlockViewMapPlugin
 {
@@ -29,17 +34,41 @@ class StoreLocatorBlockViewMapPlugin
     private $serviceRepositoryInterface;
 
     /**
+     * @var FilterBuilder
+     */
+
+    private $filterBuilder;
+    /**
+     * @var FilterGroupBuilder
+     */
+    private $filterGroupBuilder;
+
+    /**
+     * @var \Magento\Framework\Api\SortOrderBuilder
+     */
+    private $sortOrderBuilder;
+
+    /**
      * StoreLocatorBlockSearchPlugin constructor.
      * @param ServiceRepositoryInterface $serviceRepositoryInterface
      * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param FilterBuilder $filterBuilder
+     * @param FilterGroupBuilder $filterGroupBuilder
+     * @param SortOrderBuilder $sortOrderBuilder
      * @param array $data
      */
     public function __construct(
         ServiceRepositoryInterface $serviceRepositoryInterface,
-        SearchCriteriaBuilder $searchCriteriaBuilder
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        FilterBuilder $filterBuilder,
+        FilterGroupBuilder $filterGroupBuilder,
+        SortOrderBuilder $sortOrderBuilder
     ) {
         $this->serviceRepositoryInterface = $serviceRepositoryInterface;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->filterBuilder = $filterBuilder;
+        $this->filterGroupBuilder = $filterGroupBuilder;
+        $this->sortOrderBuilder = $sortOrderBuilder;
     }
 
 
@@ -79,12 +108,74 @@ class StoreLocatorBlockViewMapPlugin
      */
     public function getServiceListByRetailerId($retailerId)
     {
-        $this->searchCriteriaBuilder
-            ->addFilter(ServiceInterface::RETAILER_ID, $retailerId)
-            ->addFilter(ServiceInterface::SORT, 1)
-            ->addFilter(ServiceInterface::STATUS, 1);
+        $now = new \DateTime();
+        $currDateFormat = $now->format('Y-m-d');
 
-        $searchCriteria = $this->searchCriteriaBuilder->create();
+        $retailerIdFilter = $this->filterBuilder
+            ->setField(ServiceInterface::STATUS)
+            ->setConditionType('eq')
+            ->setValue(1)
+            ->create();
+        $retailerStatusFilter = $this->filterBuilder
+            ->setField(ServiceInterface::RETAILER_ID)
+            ->setConditionType('eq')
+            ->setValue($retailerId)
+            ->create();
+        $retailerEndDate = $this->filterBuilder
+            ->setField(ServiceInterface::END_AT)
+            ->setConditionType('gteq')
+            ->setValue($currDateFormat)
+            ->create();
+        $retailerCurrentDate = $this->filterBuilder
+            ->setField(ServiceInterface::CREATED_AT)
+            ->setConditionType('eq')
+            ->setValue($currDateFormat)
+            ->create();
+        $retailerCurrentDateEnd = $this->filterBuilder
+            ->setField(ServiceInterface::END_AT)
+            ->setConditionType('eq')
+            ->setValue($currDateFormat)
+            ->create();
+        $retailerStartDate = $this->filterBuilder
+            ->setField(ServiceInterface::CREATED_AT)
+            ->setConditionType('lteq')
+            ->setValue($currDateFormat)
+            ->create();
+        $retailerEmptyEnd = $this->filterBuilder
+            ->setField(ServiceInterface::END_AT)
+            ->setConditionType('null')
+            ->create();
+        $retailerEmptyStart = $this->filterBuilder
+            ->setField(ServiceInterface::CREATED_AT)
+            ->setConditionType('null')
+            ->create();
+        $sortOrderServices = $this->sortOrderBuilder
+            ->setField(ServiceInterface::SORT)
+            ->setDirection(SortOrder::SORT_ASC)
+            ->create();
+        $filterGroup = [
+            $this->filterGroupBuilder
+                ->addFilter($retailerIdFilter)
+                ->create(),
+            $this->filterGroupBuilder
+                ->addFilter($retailerStatusFilter)
+                ->create(),
+            $this->filterGroupBuilder
+                ->addFilter($retailerStartDate)
+                ->addFilter($retailerCurrentDate)
+                ->addFilter($retailerEmptyStart)
+                ->create(),
+            $this->filterGroupBuilder
+                ->addFilter($retailerEndDate)
+                ->addFilter($retailerCurrentDateEnd)
+                ->addFilter($retailerEmptyEnd)
+                ->create()
+        ];
+
+        $searchCriteria = $this->searchCriteriaBuilder
+            ->setFilterGroups($filterGroup)
+            ->setSortOrders([$sortOrderServices])
+            ->create();
         $searchResult = $this->serviceRepositoryInterface->getList($searchCriteria);
 
         $items = $searchResult->getItems();
